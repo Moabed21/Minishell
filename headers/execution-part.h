bfc84b9a4ac9557216f56d394fc94cf34af7e6ec
Moabed21 <moathabed21@gmail.com>
2026-03-22 13:18:32 +0300

    env struct done

--- headers/execution-part.h
+++ headers/execution-part.h
@@ -6,7 +6,7 @@
 /*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 19:15:52 by moabed            #+#    #+#             */
-/*   Updated: 2026/03/20 09:29:02 by moabed           ###   ########.fr       */
+/*   Updated: 2026/03/22 12:12:55 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,31 @@
 
 void    execution(t_cmd *cmds_list, char **envp);
 void    signals_handling();
+//env_utils
+void env_ruin(t_env *head);
+t_env *new_node(char * str);
+t_env *env_init(char **env);
+//utils
+void    shell_protection();
 
 typedef struct s_env
 {
-	char *key;
-	char *value;
+	char *variable;
 	struct s_env *next;
 }t_env;
+
+typedef struct s_exec
+{
+	//if im executing a command , what do i need ?
+	//1) fd[2] the pipe, the fork id, the return status, the env i have to loop on
+	//to execute, a file descriptor for opening and closing
+	int fd[2];
+	int fork_id;
+	int ret_stat;
+	int input_file_d;
+	int output_file_d;
+	t_env *first_env_node;
+}t_exec;
+
 
 #endif
--- headers/libs.h
+++ headers/libs.h
@@ -6,23 +6,21 @@
 /*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 19:17:10 by moabed            #+#    #+#             */
-/*   Updated: 2026/03/20 07:22:12 by moabed           ###   ########.fr       */
+/*   Updated: 2026/03/21 22:13:06 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBS_H
 #define LIBS_H
 
-// #define _DEFAULT_SOURCE
-#include "../libft/libft.h"
+//readline
+#include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+// #define _DEFAULT_SOURCE
+#include "../libft/libft.h"
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
-//main libs
 #include <signal.h>
-//signals
-//readline
 
 #endif
