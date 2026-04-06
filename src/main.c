35a969a175edd4f1c190b6dc5b2fe7e0ada78dca
Moabed21 <moathabed21@gmail.com>
2026-04-06 07:06:20 +0300

    making the main structure

--- src/main.c
+++ src/main.c
@@ -6,35 +6,38 @@
 /*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 08:50:13 by moabed            #+#    #+#             */
-/*   Updated: 2026/03/24 19:41:09 by moabed           ###   ########.fr       */
+/*   Updated: 2026/04/02 10:27:47 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/execution-part.h"
 
-int main(int ac,char **av,char **envp)
+int	main(int ac, char **av, char **envp)
 {
-    t_exec shell;
-    char *input;
-    
-    shell.last_status = 0;
-    while (1)
-    {
-        signals_handling();
-        shell_protection();
-        input = readline("minishell> ");
-        if(!input)
-            break;
-        if(global_sig !=0)
-        {
-            shell.last_status = global_sig;
-            global_sig = 0;
-        }
-        add_history(input);
-        shell.cmds = parsing(input);
-        execution(&shell, envp);
-        free(input);
-    }
-    return (shell.last_status);
-    //the return value will bw the last status of execution
+	char	*input;
+	t_env	*env;
+	t_exec	shell;
+
+	env = env_init(envp);
+	if (!env)
+		return (1);
+	shell.last_status = 0;
+	while (1)
+	{
+		shell_init(env);
+		input = readline("minishell> ");
+		if (!input)
+			break ;
+		if (global_sig != 0)
+		{
+			shell.last_status = global_sig;
+			global_sig = 0;
+		}
+		add_history(input);
+		shell.cmds = builder(input);
+		execution(&shell, env);
+		free(input);
+	}
+	return (shell.last_status);
+	// the return value will bw the last status of execution
 }
