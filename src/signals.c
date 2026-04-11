11ab7b20590006eed66a05656b620a9928811274
Moabed21 <moathabed21@gmail.com>
2026-04-11 17:48:43 +0300

    signals finished

--- src/signals.c
+++ src/signals.c
@@ -6,40 +6,55 @@
 /*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 19:23:25 by moabed            #+#    #+#             */
-/*   Updated: 2026/03/31 06:58:28 by moabed           ###   ########.fr       */
+/*   Updated: 2026/04/11 17:19:15 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//this is the signals part
+// this is the signals part
 
 #include "../headers/execution-part.h"
 
-void ctrl_c(int pid)
+void	sigint_interactive(int sig)
 {
-    write(1,"\n",1);
-    rl_on_new_line();
-    rl_replace_line("",0);
-    rl_redisplay();
+	(void)sig;
+	global_sig = 130;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
-void    do_nothing(int pid)
+void	sigquit_interactive(int sig)
 {
-    (void)pid;
-    rl_redisplay();
-    return ;
+	(void)sig;
+	rl_redisplay();
+	return ;
 }
 
-void    signals_handling()
+void	interactive_signals(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
 
-    
-    // CTRL + c
-    // signal(SIGINT, ctrl_c);
-    // // signal(); // CTRL + D (EOF)
-    // signal(SIGQUIT, do_nothing); // CTRL + "\"
+	sa_int.sa_handler = sigint_interactive;
+	sigemptyset(&sa_int.sa_mask); // this means " dont block other signals during handler"
+	sa_int.sa_flags = 0; // dont set SA_restart (important for readline)
+	sigaction(SIGINT,&sa_int,NULL);
+	
+	sa_quit.sa_handler = sigquit_interactive;
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = 0;
+	sigaction(SIGQUIT,&sa_quit,NULL);
+	rl_catch_signals = 0;  // Tell readline: "I'll handle signals myself"
+}
 
-    /*using signal function is easy but its not recommended
-    due to the differenciation
-    */
+void	default_signals()
+{
+	struct sigaction sa;
+
+	sa.sa_handler = SIG_DFL;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGQUIT,&sa,NULL);
+	sigaction(SIGINT, &sa, NULL);
 }
