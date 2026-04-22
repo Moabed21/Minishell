d207b92479a16c85911d54ae8d473d7586390923
Shatha Amarnah <shathaamarnah@Shathas-MacBook-Air-2.local>
2026-04-22 18:51:28 +0300

    everything explained

---
+++ mypart/main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shathaamarnah <shathaamarnah@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 13:55:39 by shathaamarn       #+#    #+#             */
+/*   Updated: 2026/04/20 19:01:15 by shathaamarn      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+t_cmd	*prepare_for_execution(char *input, char **env, int last_status)
+{
+	t_token	*tokens;
+	t_cmd	*cmds;
+
+	tokens = tokenization(input);
+	if (!tokens)
+		return (NULL);
+	if (check_consecutives(&tokens) == FAILURE)
+	{
+		tokenlistclear(&tokens);
+		return (NULL);
+	}
+	if (expand_tokens(tokens, env, last_status) == FAILURE)
+	{
+		tokenlistclear(&tokens);
+		return (NULL);
+	}
+	cmds = parsing(tokens);
+	tokenlistclear(&tokens);
+	return (cmds);
+}
