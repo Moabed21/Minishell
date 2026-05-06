0d22151b9fa1c1645e61185c6e73c80d5ff4a27b
Moabed21 <moathabed21@gmail.com>
2026-05-06 20:38:10 +0300

    test the projects, tests result written in main file

--- src/execution/error-handle.c
+++ src/execution/error-handle.c
@@ -6,7 +6,7 @@
 /*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 12:19:58 by moabed            #+#    #+#             */
-/*   Updated: 2026/05/05 16:53:12 by moabed           ###   ########.fr       */
+/*   Updated: 2026/05/06 17:41:46 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,4 +75,32 @@ void	free_cmds_list(t_cmd **cmds)
 		curr = next;
 	}
 	*cmds = NULL;
+}
+
+void	errmsg(char *errmsg, char *detail, int check)
+{
+	char	*msg;
+	char	*tmp;
+	
+	msg = ft_strdup("minishell: ");
+	tmp = msg;
+	msg = ft_strjoin(msg, errmsg);
+	free(tmp);
+	tmp = msg;
+	if (check)
+		msg = ft_strjoin(msg, " `");
+	else
+		msg = ft_strjoin(msg, ": ");
+	free(tmp);
+	tmp = msg;
+	msg = ft_strjoin(msg, detail);
+	free(tmp);
+	if (check)
+	{
+		tmp = msg;
+		msg = ft_strjoin(msg, "'");
+		free(tmp);
+	}
+	ft_putendl_fd(msg, STDERR_FILENO);
+	free_ptr((void **)&msg);
 }
--- src/execution/main.c
+++ src/execution/main.c
@@ -6,11 +6,34 @@
 /*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 08:50:13 by moabed            #+#    #+#             */
-/*   Updated: 2026/05/05 17:05:46 by moabed           ###   ########.fr       */
+/*   Updated: 2026/05/06 20:30:24 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/executionpart.h"
+
+t_cmd	*prepare_for_execution(char *input, char **env, int last_status)
+{
+	t_token	*tokens;
+	t_cmd	*cmds;
+
+	tokens = tokenization(input);
+	if (!tokens)
+		return (NULL);
+	if (check_if_var(&tokens) == FAILURE)
+	{
+		tokenlistclear(&tokens);
+		return (NULL);
+	}
+    if (!expand_tokens(&tokens, env, last_status))	
+    {
+		tokenlistclear(&tokens);
+		return (NULL);
+	}
+	cmds = parsing(tokens);
+	tokenlistclear(&tokens);
+	return (cmds);
+}
 
 int	main(int ac, char **av, char **envp)
 {
@@ -45,3 +68,99 @@ int	main(int ac, char **av, char **envp)
 	ruin_everything(&shell);
 	return (shell.last_status);
 }
+// Syntax cases :
+// 1) succeed except the echo $? must be 2 instead of 0
+// 2) same number 1 issue , the last status must be 2
+// 3) same issue
+// 4) same issue ,also the error message must be minishell: .: filename argument required instead of minishell: .: command not found
+// 5) succeed 100%
+// 6) the error message must be checked , also the last status must be 127 instead of 126
+// 7) the error message must be checked , last status is true
+// 8) the error message must be checked , last status is true
+
+// Signals cases :
+// 1) last status must be 130 instead of 0
+// there is issues with << end the heredoc doesnt work
+// signals last status must be checked
+
+// Env cases :
+// 1) succeed 100%
+// 2) succeed but check the real $SHELL value
+// 3) must be revised , last status true
+// 4) same as above (3)
+// 5) succedd 100%
+// 6) must print the current pwd , it print $OLDPWD , last status correct
+// 7) doesnt behave as expected at all 
+// 8) same issue as 7
+// 9) same
+
+// Echo cases
+// 1) succeed 100%
+// 2) succeed 100%
+// 3) Not required (not sure of its behaviour)
+// 4) succeed 100%
+// 5) Not required (not sure of its behaviour)
+// 6) succeed 100%
+// 7) succeed 100%
+
+// Cd cases
+// 1) must be fixed , it displays : minishell: cd: too many arguments , instead it must print / , last status 2 must be 0
+// 2) succeed 100%
+// 3) failure 100%
+// 4) failure 100%
+// 5) succeed 100%
+// 6) succeed 100%
+// 7) last status wrong must be 1 instead of 2 , else everything okay
+
+// Unset cases
+// 1) succeed 100%
+// 2) succeed 100%
+// 3) succeed 100%
+// 4) succeed 100%
+// 5) succeed 100%
+
+// Exit cases
+// 1) succeed 100%
+// 2) succeed 95% (make a quick check)
+// 3) succeed 100%
+// 4) last status must be 2 instead of 0
+// 5) last status must be 1 instead of 2
+// 6) succeed 100%
+// 7) last status must be 1 instead of 2
+// 8) succeed 100%
+// 9) last status must be 1 instead of 2
+
+// Extra cases
+// 1) succeed 100%
+// 2) Unknown
+// 3) succeed 90%
+
+// Pipe cases
+// 1) succeed 100%
+// 2) succeed 100%
+// 3) succeed 100%
+// 4) succeed 90% (WE SHOULDNT PRINT EXIT)
+// 5) succeed 90% (WE SHOULDNT PRINT EXIT)
+// 6) succeed 100%
+// 7) succeed 100%
+// 8) succeed 100%
+// 9) succeed 100%
+
+// Redir cases
+// 1) succeed 100%
+// 2) succeed 100%
+// 3) failed 100% (causes segfault)
+// 4) recheck , last status true
+// 5) failed 100% (causes segfault)
+// 6) failed 100% (causes segfault)
+// 7) succeed 100%
+
+// Expander cases
+// 1) failure 90% (last status true)
+// 2) recheck the error message we display minishell: “ls: command not found , the real one minishell: ls -l: Command not found
+// 3) recheck (last status false)
+// 4) recheck (last status false)
+// 5) failure 100%
+// 6) recheck
+
+//
